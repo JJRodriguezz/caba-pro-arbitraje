d3994f67a24e276c8443f60944d89412c379152f
JuanJose <jjrodrigur@eafit.edu.co>
2025-08-27 22:40:23 -0500

    feat(data): carga de datos iniciales con CommandLineRunner

---
+++ src/test/java/com/caba/caba_pro/CabaProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caba.caba_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabaProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
