5a04177f1ab3716ea9d409d940d5d4356c212ce5
Isa idarraga <iidarragab@eafit.edu.co>
2025-09-05 00:31:31 -0500

    style: aplicar formato Google Java Format a archivos principales

--- src/main/java/com/caba/caba_pro/CabaProApplication.java
+++ src/main/java/com/caba/caba_pro/CabaProApplication.java
@@ -1,51 +1,12 @@
 package com.caba.caba_pro;
 
-import com.caba.caba_pro.domain.Referee;
-import com.caba.caba_pro.repository.RefereeRepository;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CabaProApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CabaProApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner initDatabase(RefereeRepository refereeRepository) {
-        return args -> {
-            refereeRepository.save(Referee.builder()
-                    .firstName("Isabella")
-                    .lastName("Idarraga")
-                    .idNumber("12345")
-                    .phone("3001234567")
-                    .email("isa@caba.com")
-                    .specialty("CAMPO")
-                    .level("FIBA")
-                    .build());
-
-            refereeRepository.save(Referee.builder()
-                    .firstName("Juan")
-                    .lastName("Rodríguez")
-                    .idNumber("66666")
-                    .phone("3019876543")
-                    .email("juan@caba.com")
-                    .specialty("MESA")
-                    .level("PRIMERA")
-                    .build());
-
-            refereeRepository.save(Referee.builder()
-                    .firstName("Diego")
-                    .lastName("Gonzalez")
-                    .idNumber("00001")
-                    .phone("3019876543")
-                    .email("diego@caba.com")
-                    .specialty("LINEA")
-                    .level("PRIMERA")
-                    .build());
-        };
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(CabaProApplication.class, args);
+  }
 }
